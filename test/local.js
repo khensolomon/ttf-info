10b53df3d3eb305765df7777329a803fd83a3680
Khen Solomon Lethil <khensolomon@gmail.com>
2021-01-31 16:20:17 +0100

    testing

--- test/local.js
+++ test/local.js
@@ -1,15 +1,20 @@
 // import fs from 'fs';
 import ttfMeta from '../lib/index.js';
+
+// response = await this.view_response(e.tables.name);
 // import ttfMeta from '../index.js';
 // ttfMeta.ttfInfo('./test/font.ttf', function(err, info) {
 //       console.log('error',err);
 //       console.log('info',info);
 // });
-
-// ttfMeta.promise('./test/font.ttf').then(
-//       e=>console.log('result',e)
+var fontFile = '/storage/media/fonts/secondary/winuniiw.ttf';
+// var fontFile = '/storage/media/fonts/secondary/m-myanmar1.TTF';
+// var fontFile = '/Windows/Fonts/AdobeHeitiStd-Regular.otf';
+// var fontFile = '/Windows/Fonts/BirchStd.otf';
+// ttfMeta.promise(fontFile).then(
+//   e => console.log(e.meta)
 // ).catch(
-//       e=>console.log('error',e)
+//   e=>console.log('error',e)
 // )
 
 // fs.readFile('/storage/media/fonts/external/BURMA___.TTF',function(err, buffer) {
