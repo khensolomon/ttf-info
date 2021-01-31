1ea4794d1d94a398aeef86e778f50a3c75abe017
Khen Solomon Lethil <khensolomon@gmail.com>
2021-01-31 11:51:40 +0100

    none from abc

--- test/index.none.js
+++ test/index.none.js
@@ -1,18 +1,18 @@
-import fs from 'fs';
-import ttfMeta from '../lib/index.js';
+// import fs from 'fs';
+// import ttfMeta from '../lib/index.js';
 // import ttfMeta from '../index.js';
-// ttfMeta.ttfInfo('/storage/media/fonts/external/BURMA___.TTF', function(err, info) {
+// ttfMeta.ttfInfo('./test/font.ttf', function(err, info) {
 //       console.log('error',err);
 //       console.log('info',info);
 // });
 
 
 
-ttfMeta.promise('/storage/media/fonts/external/-BURMA___.TTF').then(
-      e=>console.log('result',e)
-).catch(
-      e=>console.log('error',e)
-)
+// ttfMeta.promise('./test/font.ttf').then(
+//       e=>console.log('result',e)
+// ).catch(
+//       e=>console.log('error',e)
+// )
 
 // fs.readFile('/storage/media/fonts/external/BURMA___.TTF',function(err, buffer) {
 //       if (err){
