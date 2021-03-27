5391649d0ec00de8b56e11847ae8ce5e691ef590
Khen Solomon Lethil <khensolomon@gmail.com>
2021-03-27 14:21:56 +0100

    change filepath

--- test/local.js
+++ test/local.js
@@ -9,30 +9,50 @@ import ttfMeta from '../lib/index.js';
 // });
 // var fontFile = '/storage/media/fonts/secondary/winuniiw.ttf';
 // var fontFile = './assets/font/Myanmar3.ttf';
-var fontFile = './assets/font/notation.ttf';
+var fontFile = './assets/font/ttfmeta.ttf';
 // var fontFile = '/storage/media/fonts/secondary/m-myanmar1.TTF';
 // var fontFile = '/Windows/Fonts/AdobeHeitiStd-Regular.otf';
 // var fontFile = '/Windows/Fonts/BirchStd.otf';
-// ttfMeta.promise(fontFile).then(
-//   e => console.log(e)
-// ).catch(
-//   e=>console.log('error',e)
-// )
 
-console.log('abc');
+/**
+ * send file
+ */
+ttfMeta.promise(fontFile).then(
+  e => console.log(e)
+).catch(
+  e=>console.log('error',e)
+)
+
 // fs.readFile(fontFile,function(err, buffer) {
-
 //   console.log('error',err);
 //   console.log('info',buffer);
 // })
 
-// fs.readFile(fontFile,function(err, buffer) {
-//   if (err){
-//     console.log('err',err);
-//   } else {
-//     ttfMeta.ttfInfo(buffer,function(err,info){
-//       console.log('error',err);
-//       console.log('info',info);
-//     })
-//   }
-// })
+/**
+ * custom read and send buffer for callback
+ */
+fs.readFile(fontFile,function(err, buffer) {
+  if (err){
+    console.log('err',err);
+  } else {
+    ttfMeta.ttfInfo(buffer,function(err,info){
+      console.log('error',err);
+      console.log('info',info);
+    })
+  }
+})
+
+/**
+ * custom read and send buffer for promise
+ */
+fs.readFile(fontFile,function(err, buffer) {
+  if (err){
+    console.log('err',err);
+  } else {
+    ttfMeta.promise(buffer).then(
+      e => console.log(e)
+    ).catch(
+      e=>console.log('error',e)
+    )
+  }
+})
